4dd0352e555f4e61581c56a0138b422c4bfcc4b9
Jevgeni Tarasov <jevgeni@hey.com>
2021-05-02 13:53:56 +0200

    More tweaks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import * as lobos from 'lobos'
 import './App.css'
 import { dayFractionNow } from '../lib/time'
 
-const ACTIVATION_PERIOD = 80
+const ACTIVATION_PERIOD = 750
 
 interface AppProps {}
 
@@ -24,7 +24,7 @@ class App extends React.Component<AppProps, AppState> {
   private readonly numberOfCols: number
   private readonly numberOfPoints: number
   private readonly safetyFactor: number = 1.2
-  private readonly cellDenominator: number = 24
+  private readonly cellDenominator: number = 13
 
   private activationPointIndex: number = 0
   private decayPointIndex: number = 0
@@ -75,10 +75,10 @@ class App extends React.Component<AppProps, AppState> {
     const foregroundSaturation = waveMap(t, 0.3, 1, 0.5) + 0.3;
     const backgroundLightness = waveMap(t, 0.5, 1, 0.5) + 0.1
     const backgroundAlpha = waveMap(t, 0.025, 1, 0.5) + 0.4
-    const backgroundSaturation = waveMap(t, 0.1, 1, 0.5) + 0.25
+    const backgroundSaturation = waveMap(t, 0.2, 1, 0.5) + 0.2
     const hue = waveMap(value, 310, 22.5, 45)
     return {
-      foreground: new HslaColor(hue, foregroundSaturation, foregroundLightness, 0.8),
+      foreground: new HslaColor(hue, foregroundSaturation, foregroundLightness, 0.3),
       background: new HslaColor(hue, backgroundSaturation, backgroundLightness, backgroundAlpha)
     }
   }
@@ -110,13 +110,16 @@ class App extends React.Component<AppProps, AppState> {
         </div>
         <main id="main-content" className="content" style={{
           position: "absolute",
-          top: `${this.cellHeight*3}px`,
-          left: `${this.cellWidth*2}px`,
-          width: `${this.cellWidth*9}px`,
-          height: `${this.cellHeight*9}px`,
+          top: `${this.cellHeight*1}px`,
+          left: `${this.cellWidth*1}px`,
+          width: `${this.cellWidth*5}px`,
+          height: `${this.cellHeight*5}px`,
         }}>
           <h1 className="title">BITBAU</h1>
-          <p>Hi, my name is Jev. I build products. This is a place where I put my tech stuff.</p>
+          <p>Hi, my name is Jev. I live in Switzerland and create digital systems for banks and
+             other financial institutions.</p>
+
+          <h2>Me on the Internet</h2>
           <p><a href="https://github.com/devjev">Github Profile</a></p>
           <p><a href="https://jevgeni.blog/">Blog</a></p>
         </main>
@@ -124,31 +127,44 @@ class App extends React.Component<AppProps, AppState> {
     )
   }
 
+  updateBaseColors() {
+    // Update background color based on time of the day
+    const t = dayFractionNow()
+    const lightnessBody = waveMap(t, 0.985, 1, 0.5) + 0.015
+    const backgroundColor = new HslaColor(0, 0, lightnessBody, 1)
+    document.body.style.backgroundColor = backgroundColor.toColorString()
+
+    // Update content area
+    const lightnessContent = waveMap(t, 0.9, 1, 0.5) + 0.1
+    const lightnessCopy = waveMap(t, 0.9, 1, 0) + 0.1
+    const backgroundColorContent = new HslaColor(0, 0, lightnessContent, 0.6)
+    const copyColor = new HslaColor(0, 0, lightnessCopy, 0.77)
+    if (document.getElementById("main-content")) {
+      // @ts-ignore
+      document.getElementById("main-content").style.backgroundColor = backgroundColorContent.toColorString()
+      // @ts-ignore
+      document.getElementById("main-content").style.color = copyColor.toColorString()
+    }
+  }
+
+  updateCellColors() {
+    this.getActivationPoints(9).then((sobolPoints) => {
+      const rows = sobolPoints.map(([_x, y]) => Math.floor(y * this.numberOfRows))
+      const cols = sobolPoints.map(([x, _y]) => Math.floor(x * this.numberOfCols))
+      this.setState({ ...this.state, activationBroadcast: { rows, cols } })
+    })
+  }
+
   componentDidMount() {
+    // Initial cycle 5 times to get a nice initial picture
+    for (let i = 0; i < 5; i++) {
+      this.updateCellColors()
+    }
+    this.updateBaseColors()
+
     const activationTimer = setInterval(() => {
-      this.getActivationPoints(9).then((sobolPoints) => {
-        const rows = sobolPoints.map(([_x, y]) => Math.floor(y * this.numberOfRows))
-        const cols = sobolPoints.map(([x, _y]) => Math.floor(x * this.numberOfCols))
-        this.setState({ ...this.state, activationBroadcast: { rows, cols } })
-      })
-
-      // Update background color based on time of the day
-      const t = dayFractionNow()
-      const lightnessBody = waveMap(t, 0.985, 1, 0.5) + 0.015
-      const backgroundColor = new HslaColor(0, 0, lightnessBody, 1)
-      document.body.style.backgroundColor = backgroundColor.toColorString()
-
-      // Update content area
-      const lightnessContent = waveMap(t, 0.9, 1, 0.5) + 0.1
-      const lightnessCopy = waveMap(t, 0.9, 1, 0) + 0.1
-      const backgroundColorContent = new HslaColor(0, 0, lightnessContent, 0.4)
-      const copyColor = new HslaColor(0, 0, lightnessCopy, 0.77)
-      if (document.getElementById("main-content")) {
-        // @ts-ignore
-        document.getElementById("main-content").style.backgroundColor = backgroundColorContent.toColorString()
-        // @ts-ignore
-        document.getElementById("main-content").style.color = copyColor.toColorString()
-      }
+      this.updateCellColors()
+      this.updateBaseColors()
     }, ACTIVATION_PERIOD)
 
     this.setState({ ...this.state, activationTimer })
